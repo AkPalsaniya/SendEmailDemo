3d1f8a2a541b2e39166f75be7371c9576fe6c4ff
AkPalsaniya <abidalipalsaniya8@gmail.com>
2024-03-19 14:07:11 +0530

    Changes Applied

--- sendEmailDemo/mailDemo.aspx.cs
+++ sendEmailDemo/mailDemo.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
+using System.Security.Principal;
 using System.Text;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
 namespace sendEmailDemo
 {
     public partial class mailDemo : System.Web.UI.Page
@@ -18,7 +22,16 @@ namespace sendEmailDemo
                 // Sender's email credentials for Gmail
                 string senderEmail = "xyz@gmail.com";
                 //Check for two-factor authentication If you have enabled two-factor authentication for the sender's email account, you may need to generate an application-specific password to use in your code instead of the account's actual password.
-                string senderPassword = "ylwx 000 xmgn hmld";
+                string senderPassword = "ylwx 000 xmgn hmld"; //application specific password insted of Account Password
+
+                //Create application specific password
+                // 1.Go to your Google Account.
+                // 2.Select Security.
+                // 3.Under "Signing in to Google," select 2 - Step Verification.
+                // 4.At the bottom of the page, select App passwords.
+                // 5.Enter a name that helps you remember where you'll use the app password.
+                // 6.Select Generate.
+
 
                 // Create and configure SMTP client for Gmail
                 SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
@@ -32,8 +45,8 @@ namespace sendEmailDemo
                 // Create the email message
                 MailMessage mailMessage = new MailMessage(senderEmail, "ReceverEmail@gmail.com")
                 {
-                    Subject = txtSubject.Text,
-                    Body = txtMsg.Text,
+                    Subject = txtSubject.Text, //Your Subject Text
+                    Body = txtMsg.Text, //Your Message
                     IsBodyHtml = true
                 };
 
