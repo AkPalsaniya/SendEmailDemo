c50a6d97922cb450343670845eee2ace83cdc755
AkPalsaniya <abidalipalsaniya8@gmail.com>
2024-03-19 12:58:49 +0530

    Add project files.

---
+++ sendEmailDemo/mailDemo.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="mailDemo.aspx.cs" Inherits="sendEmailDemo.mailDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            Subject <asp:TextBox ID="txtSubject" runat="server"></asp:TextBox><br />
+            Message <asp:TextBox ID="txtMsg" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnSent" runat="server" Text="Send" OnClick="btnSent_Click"/>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ sendEmailDemo/mailDemo.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+namespace sendEmailDemo
+{
+    public partial class mailDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSent_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Sender's email credentials for Gmail
+                string senderEmail = "xyz@gmail.com";
+                //Check for two-factor authentication If you have enabled two-factor authentication for the sender's email account, you may need to generate an application-specific password to use in your code instead of the account's actual password.
+                string senderPassword = "ylwx 000 xmgn hmld";
+
+                // Create and configure SMTP client for Gmail
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false, 
+                    Credentials = new NetworkCredential(senderEmail, senderPassword)
+                };
+
+                // Create the email message
+                MailMessage mailMessage = new MailMessage(senderEmail, "ReceverEmail@gmail.com")
+                {
+                    Subject = txtSubject.Text,
+                    Body = txtMsg.Text,
+                    IsBodyHtml = true
+                };
+
+                // Send the email
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to send email. Error: " + ex.Message);
+            }
+
+            
+        }
+    }
+}
